089630028d8b8c941a2ee9e61c9cb5ae27f943ca
taroj1205 <taroj1205@gmail.com>
2024-11-09 14:45:04 +1300

    feat: implement text highlighting in clipboard history and simplify state management

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,32 @@ type ClipboardHistory = {
   image: string;
 };
 
+const Highlight = ({ key, children }: { key: number; children: string }) => {
+  return (
+    <span data-key={key} class="bg-yellow-500/30 text-white">
+      {children}
+    </span>
+  );
+};
+
+const highlightText = (text: string, searchText: string) => {
+  if (!searchText) return text;
+  const parts = text.split(new RegExp(`(${searchText})`, "gi"));
+  return parts.map((part, i) =>
+    part.toLowerCase() === searchText.toLowerCase() ? (
+      // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+      <Highlight key={i}>{part}</Highlight>
+    ) : (
+      part
+    )
+  );
+};
+
 export const App = ({ db_path }: { db_path: string }) => {
   const [activeIndex, setActiveIndex] = createSignal(0);
   const [clipboardHistory, setClipboardHistory] = createSignal<
-    Record<string, ClipboardHistory[]>
-  >({});
+    ClipboardHistory[]
+  >([]);
   const [offset, setOffset] = createSignal(0);
   const limit = 20;
   let inputRef: HTMLInputElement | undefined;
@@ -30,17 +51,13 @@ export const App = ({ db_path }: { db_path: string }) => {
   let scrollAreaRef: HTMLDivElement | undefined;
 
   const handleKeyDown = (event: KeyboardEvent) => {
-    const totalLength = Object.values(clipboardHistory()).reduce(
-      (acc, curr) => acc + curr.length,
-      0
-    );
+    const totalLength = clipboardHistory().length;
     if (event.key === "ArrowDown" && totalLength > 0) {
       setActiveIndex((prev) => Math.min(prev + 1, totalLength - 1));
     } else if (event.key === "ArrowUp" && totalLength > 0) {
       setActiveIndex((prev) => Math.max(prev - 1, 0));
     } else if (event.key === "Enter") {
-      const item =
-        clipboardHistory()[Object.keys(clipboardHistory())[0]][activeIndex()];
+      const item = clipboardHistory()[activeIndex()];
       handleCopy(item);
       getCurrentWindow().hide();
     } else if (event.key === "Escape") {
@@ -71,10 +88,7 @@ export const App = ({ db_path }: { db_path: string }) => {
           block: "center",
         });
       }
-      const totalLength = Object.values(clipboardHistory()).reduce(
-        (acc, curr) => acc + curr.length,
-        0
-      );
+      const totalLength = clipboardHistory().length;
       if (activeIndex() >= totalLength - 5) {
         setOffset((prev) => prev + limit);
         updateHistory(offset(), limit);
@@ -99,23 +113,7 @@ export const App = ({ db_path }: { db_path: string }) => {
       limit,
       filter: { content: inputRef?.value },
     }).then((history) => {
-      const newHistory = history.reduce((acc, item) => {
-        const relativeTime = getRelativeTime(new Date(item.date));
-        if (!acc[relativeTime]) {
-          acc[relativeTime] = [];
-        }
-        acc[relativeTime].push(item);
-        return acc;
-      }, {} as Record<string, ClipboardHistory[]>);
-      setClipboardHistory((prev) => ({
-        ...prev,
-        ...Object.fromEntries(
-          Object.entries(newHistory).map(([key, value]) => [
-            key,
-            [...(prev[key] || []), ...value],
-          ])
-        ),
-      }));
+      setClipboardHistory((prev) => [...prev, ...history]);
     });
   };
 
@@ -141,19 +139,7 @@ export const App = ({ db_path }: { db_path: string }) => {
       db_path,
       filter: { content: inputRef?.value },
     }).then((items) => {
-      if (items.length === 0) {
-        setClipboardHistory({});
-        return;
-      }
-      const newHistory = items.reduce((acc, item) => {
-        const relativeTime = getRelativeTime(new Date(item.date));
-        if (!acc[relativeTime]) {
-          acc[relativeTime] = [];
-        }
-        acc[relativeTime].push(item);
-        return acc;
-      }, {} as Record<string, ClipboardHistory[]>);
-      setClipboardHistory(newHistory);
+      setClipboardHistory(items);
     });
   };
 
@@ -162,15 +148,7 @@ export const App = ({ db_path }: { db_path: string }) => {
     invoke<ClipboardHistory[]>("get_history", {
       db_path,
     }).then((history) => {
-      const newHistory = history.reduce((acc, item) => {
-        const relativeTime = getRelativeTime(new Date(item.date));
-        if (!acc[relativeTime]) {
-          acc[relativeTime] = [];
-        }
-        acc[relativeTime].push(item);
-        return acc;
-      }, {} as Record<string, ClipboardHistory[]>);
-      setClipboardHistory(newHistory);
+      setClipboardHistory(history);
     });
   };
 
@@ -217,52 +195,63 @@ export const App = ({ db_path }: { db_path: string }) => {
             class="h-full pb-2 overflow-y-auto invisible hover:visible max-h-[calc(100svh-4.5rem)] hover:overflow-y-auto select-none scroll-area"
           >
             <ul ref={listRef} class="visible w-full">
-              {Object.keys(clipboardHistory()).length === 0 && (
+              {clipboardHistory().length === 0 && (
                 <p class="text-lg whitespace-pre overflow-auto p-2">
                   No content available
                 </p>
               )}
-              <For each={Object.entries(clipboardHistory())}>
-                {([time, items]) => (
-                  <>
-                    <li class="text-gray-400 text-sm p-2">{time}</li>
-                    <For each={items}>
-                      {(item, index) => (
-                        <li class="w-full">
-                          <button
-                            type="button"
-                            onDblClick={() => handleCopy(item)}
-                            onClick={() => handleClick(index())}
-                            class={cn(
-                              "cursor-pointer w-full grid grid-cols-[auto_1fr] gap-2 p-2 h-10 rounded truncate overflow-hidden place-items-center",
-                              {
-                                "bg-active": index() === activeIndex(),
-                              }
-                            )}
-                          >
-                            {item.type_ === "image" ? (
-                              <>
-                                <ImageIcon class="size-4" />
-                                <img
-                                  src={`data:image/png;base64,${item.image}`}
-                                  alt="clipboard content"
-                                  class="h-full w-full object-cover overflow-hidden"
-                                />
-                              </>
-                            ) : (
-                              <>
-                                <DocumentIcon class="size-4" />
-                                <p class="w-full overflow-hidden text-left text-ellipsis">
-                                  {item.content.trim().split("\n")[0]}
-                                </p>
-                              </>
-                            )}
-                          </button>
-                        </li>
+              <For each={clipboardHistory()}>
+                {(item, index) => {
+                  const currentDate = getRelativeTime(new Date(item.date));
+                  const prevDate =
+                    index() > 0
+                      ? getRelativeTime(
+                          new Date(clipboardHistory()[index() - 1].date)
+                        )
+                      : null;
+
+                  return (
+                    <>
+                      {(index() === 0 || currentDate !== prevDate) && (
+                        <li class="text-gray-400 text-sm p-2">{currentDate}</li>
                       )}
-                    </For>
-                  </>
-                )}
+                      <li class="w-full">
+                        <button
+                          type="button"
+                          onDblClick={() => handleCopy(item)}
+                          onClick={() => handleClick(index())}
+                          class={cn(
+                            "cursor-pointer w-full grid grid-cols-[auto_1fr] gap-2 p-2 h-10 rounded truncate overflow-hidden place-items-center",
+                            {
+                              "bg-active": index() === activeIndex(),
+                            }
+                          )}
+                        >
+                          {item.type_ === "image" ? (
+                            <>
+                              <ImageIcon class="size-4" />
+                              <img
+                                src={`data:image/png;base64,${item.image}`}
+                                alt="clipboard content"
+                                class="h-full w-full object-cover overflow-hidden"
+                              />
+                            </>
+                          ) : (
+                            <>
+                              <DocumentIcon class="size-4" />
+                              <p class="w-full overflow-hidden text-left text-ellipsis">
+                                {highlightText(
+                                  item.content.trim().split("\n")[0],
+                                  inputRef?.value || ""
+                                )}
+                              </p>
+                            </>
+                          )}
+                        </button>
+                      </li>
+                    </>
+                  );
+                }}
               </For>
             </ul>
           </div>
@@ -324,13 +313,13 @@ export const App = ({ db_path }: { db_path: string }) => {
                     />
                   </div>
                 ) : (
-                  <textarea
-                    class="h-full scroll-area w-full max-h-[390px] resize-none bg-primary outline-none"
-                    value={
+                  <div class="h-full scroll-area w-full max-h-[390px] overflow-auto whitespace-pre-wrap bg-primary p-2">
+                    {highlightText(
                       Object.values(clipboardHistory()).flat()[activeIndex()]
-                        ?.content || ""
-                    }
-                  />
+                        ?.content || "",
+                      inputRef?.value || ""
+                    )}
+                  </div>
                 )}
               </>
             )}
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,8 +1,9 @@
 export const getRelativeTime = (date: Date): string => {
-  const formatter = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
-  const daysDifference = Math.floor(
-    (date.getTime() - Date.now()) / (1000 * 60 * 60 * 24)
-  );
+  const formatter = new Intl.RelativeTimeFormat("en", {
+    numeric: "auto",
+  });
+  const daysDifference =
+    Math.floor((date.getTime() - Date.now()) / (1000 * 60 * 60 * 24)) + 1;
   const relativeTime = formatter.format(daysDifference, "day");
   return relativeTime.charAt(0).toUpperCase() + relativeTime.slice(1);
 };
