b00469707cdaff1af547a882c2efd776790c95b8
taroj1205 <taroj1205@gmail.com>
2024-11-15 21:48:16 +1300

    fix: update version sync script to match tauri.conf.json structure

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -10,7 +10,7 @@ const tauriConfigPath = join('src-tauri', 'tauri.conf.json');
 const tauriConfig = JSON.parse(readFileSync(tauriConfigPath, 'utf8'));
 
 // Update version in tauri.conf.json
-tauriConfig.package.version = version;
+tauriConfig.version = version;
 
 // Write back to tauri.conf.json
 writeFileSync(tauriConfigPath, JSON.stringify(tauriConfig, null, 2) + '\n');
